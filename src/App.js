f633fb81dafbfb2f3a12d4b51dbb8656aca3e204
lukostudy <lukostudy@gmail.com>
2023-12-14 12:16:24 +0100

    fixed br tags in jsx

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there!!! <br>
-          This is new feature <br>
+          Hello there!!! <br/>
+          This is new feature <br/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
